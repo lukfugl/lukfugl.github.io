6bf432a49e16d5d4c1138e08ea2fb319b4fd23e4
Jacob Fugal <lukfugl@gmail.com>
2020-11-21 11:03:24 -0700

    start no-thanks

---
+++ no-thanks/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const fetch = require('fetch');
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: "Loading..." };
+    fetch('http://67.2.26.52:8080/')
+      .then(response => response.text())
+      .then(text => this.setState({ text }));
+  }
+
+  render() {
+    return (<div>{this.state.text}</div>);
+  }
+}
+
+const domContainer = document.querySelector('#app');
+ReactDOM.render(<App />, domContainer);
